7abd319dcc3dcdcdec6110403eaebb262c1ff42d
Cory Zauss <coryclassic1989@gmail.com>
2022-06-22 14:36:21 -0400

    comments

--- server.go
+++ server.go
@@ -20,3 +20,7 @@ func main() {
 }
 
 //! we deine two different handlers. these handlers respond to any HTTP requests that matchs the string pattern we define as the first parameter
+// whenever a request is made for the home page or localhost:8081/
+// the first handler will respond as the query matchs that pattern
+
+//? to run our server  type " go run server.go " into the terminal then head to  http://localhost:8081/world 
